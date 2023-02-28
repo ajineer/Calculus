033b432acf2cf8dadb4524705348e51b85db621e
Mark <ajineer@outlook.com>
2023-02-27 21:19:43 -0700

    cleaned up some of the formatting, added collapsible function to some of the excercises.

--- scripts/buttonOpen.js
+++ scripts/buttonOpen.js
@@ -1,15 +1,25 @@
-var coll = document.getElementsByClassName("solButton");
-var i;
-
-for(i = 0; i<coll.length; i++){
-    coll[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        var Two_3_1sol = this.nextElementSibling;
-        if (SVGTextContentElement.style,display === "block"){
-            SVGTextContentElement.style.display = "none"
-        } else{
-            Two_3_1sol.style.display = "block";
+var btn = document.getElementsByClassName("Two_3");
+    
+for(var i = 0; i<btn.length; i++){
+    btn[i].addEventListener("click", function () {
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
         }
     });
+}
 
+var btn = document.getElementsByClassName("Two_4");
+    
+for(var i = 0; i<btn.length; i++){
+    btn[i].addEventListener("click", function () {
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
